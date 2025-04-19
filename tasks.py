3951de38d1ae44b87339703d0442e495878aceab
George Poliovei <poliovei@gmail.com>
2025-04-19 16:21:47 -0300

    Initial local commit

---
+++ tasks.py
@@ -0,0 +1,15 @@
+from celery import Celery
+from time import sleep
+
+app = Celery("tasks", broker="redis://localhost:6379", backend="redis://localhost:6379")
+
+@app.task
+def process(x, y):
+
+    i = 0
+    while i < 5:
+        sleep(1)
+        i += 1
+        print(f"process {i} ...")
+
+    return x**2 + y**2
